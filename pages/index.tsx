4edc4ae9e57e52ae0d73fc8743da7eeda55fbe11
Mahdi Aryayi <mahdiaryayi@gmail.com>
2021-06-27 23:49:07 +0430

    Added ant boilerplate

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,16 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { Layout } from 'antd';
+
+const { Header, Footer, Sider, Content } = Layout;
 
 export default function Home() {
   return (
-    <div>
-      Parsnevesht
-    </div>
+    <Layout>
+      <Header>Header</Header>
+      <Content>Content</Content>
+      <Footer>Footer</Footer>
+    </Layout>
   )
 }
